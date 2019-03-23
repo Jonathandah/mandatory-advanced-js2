ead4e9df86a464247f49d316c6915dce2aa4e66b
Jonathan Dahnberg <Jonathan.MeeprongDahnberg@utb.ecutbildning.se>
2019-03-23 16:11:46 +0100

    update on edit

--- movies/src/edit.js
+++ movies/src/edit.js
@@ -14,7 +14,10 @@ class Edit extends Component {
       description: "",
       director: "",
       rating: 0,
+      error: "",
     }
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   componentDidMount(){
@@ -34,12 +37,44 @@ class Edit extends Component {
     })
   }
   onChange(e){
-    //this.setState()
-  }
+    console.log(e.target.value);
+    console.log(e.target.id);
+    let id = e.target.id; 
+    if(id === "title"){
+        this.setState({title: e.target.value})
+        console.log(this.state)
+    }else if(id === "description"){
+        this.setState({description: e.target.value})
+        console.log(this.state)
+    }else if(id === "director"){
+        this.setState({director: e.target.value})
+        console.log(this.state)
+    }else if(id === "rating"){
+        this.setState({rating: e.target.value})
+        console.log(this.state)
+    }
+}
+onSubmit(e){
+  e.preventDefault();
+  let id = this.props.match.params.id;
+  console.log(id);
+  let obj = {};
 
-  onSubmit(){
-
-  }
+  obj.title = this.state.title 
+  obj.description = this.state.description
+  obj.director = this.state.director
+  obj.rating = this.state.rating
+  console.log(obj)
+  let stringified = JSON.stringify(obj);
+  console.log(stringified);
+  axios.put("http://ec2-13-53-132-57.eu-north-1.compute.amazonaws.com:3000/movies/", id, obj, {headers: {"Content-Type": "application/json"}})
+  .then((respons)=>{
+      console.log(respons);
+  })
+  .catch((error)=>{
+    console.log(error);
+  })
+}
 
   render() {
     return (
